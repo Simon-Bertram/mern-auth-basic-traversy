28611b089044c0f2f8e7225798f58da70a2bd888
simon-bertram <simonbertram@hotmail.com>
2023-12-22 09:59:07 +0000

    Setup Morgan for logging

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import connectDB from './config/db.js';
+import morgan from 'morgan';
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
