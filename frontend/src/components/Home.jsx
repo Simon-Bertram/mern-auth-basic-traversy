95014c6ba849fc2fc0cbe5492e256f96f7a083ef
simon-bertram <simonbertram@hotmail.com>
2023-12-27 12:19:28 +0000

    Style the cards on the homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,24 @@
+import productList from '../data/productList.json'
+
 const Home = () => {
   return ( 
     <div className="container mx-auto">
-      <h1 className="text-4xl">Mern Auth Basic App</h1>
-      <p>Login functionality, users saved to mongoDB, Redux to manage state</p>
+      <div className='grid grid-cols-3 gap-8'>
+        {productList.products.map((product) => (
+          <div key={product.id} className="card h-96 border-4 mb-8">
+            <div className='card h-60 flex flex-col gap-12 py-10'>
+              <div className='mx-auto'>
+                <img src={product.imageUrl} alt="" className='h-40 w-40 object-scale-down' />
+              </div>
+              <div className='text-center'>
+                <h5>{product.name}</h5>
+                <p>£{product.price}</p>
+                <button className='btn bg-blue-800 text-white mt-4'>Add to cart</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
    );
 }
