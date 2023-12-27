a81203c1c6ea3042fa20bb3b0e7a39733e2c1a4d
simon-bertram <simonbertram@hotmail.com>
2023-12-27 16:49:56 +0000

    Create user routes and empty functions in userController

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+import { 
+  authUser ,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.route('/auth').post(authUser);
+router.post('/auth', authUser);
+router.post('/register', registerUser);
+router.post('/logout', logoutUser);
+router.get('/profile', getUserProfile);
+router.put('/profile', updateUserProfile);
 
 export default router;
