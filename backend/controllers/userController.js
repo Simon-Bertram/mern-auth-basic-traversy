e84d0d74ea6bed4b3ad1a910d8bc82f60b1493fa
simon-bertram <simonbertram@hotmail.com>
2023-12-27 21:32:20 +0000

    Create controller logic for authenticating and registering new users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,59 @@
 import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
 
 // @desc  Authenticate user & get token
 // @route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'TODO: Auth user & get token' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // @desc   Register a new user
 // route   POST /api/users/register
 // access  Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'TODO: Register user' });
+  const { name, email, password } = req.body;
+
+  // Check if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // Create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    // If user is created, return 201 and the user object
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    // If user is not created, return 400 and throw an error
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc   Get user profile
