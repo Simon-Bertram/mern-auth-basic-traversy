6cf1e8f124bab72e11d49735c30c9de91497e901
simon-bertram <simonbertram@hotmail.com>
2023-12-28 00:16:20 +0000

    Replace login button in navbar with user profile button if user is logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Header"
 import { Outlet } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
